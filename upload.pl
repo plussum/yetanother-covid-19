64b1572566b3ba6d33308ace50358bbf60e50148
plussum <pls_git@outlook.com>
2021-01-18 07:25:23 +0900

    update

--- upload.pl
+++ upload.pl
@@ -8,6 +8,17 @@ use warnings;
 
 use config;
 
+my $CODE = 0;
+my $WEB = 0;
+for(@ARGV){
+	$CODE = 1 if(/-code/);
+	$WEB = 1 if(/-web/);
+}
+if($CODE == 0 && $WEB == 0){
+	$CODE = 1;
+	$WEB = 1;
+}
+
 # system("./upload $config::WIN_PATH master");
 #system("./upload $config::WIN_PATH develop");
 
